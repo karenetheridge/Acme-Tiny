7663f0633db8ae5be883d6d2b150ef34f7e2f283
Karen Etheridge <ether@cpan.org>
2013-04-21 14:13:44 -0700

    document the insanity; add images

--- lib/Tiny/Tiny.pm
+++ lib/Tiny/Tiny.pm
@@ -1,8 +1,7 @@
 use strict;
 use warnings;
 package Tiny::Tiny;
-# ABSTRACT: ...
-
+# ABSTRACT: minimal minimalism
 
 
 1;
@@ -12,20 +11,16 @@ __END__
 
 =head1 SYNOPSIS
 
-...
+    use Tiny::Tiny;
 
 =head1 DESCRIPTION
 
+This module does the tiniest thing of all -- nothing!
 
-=head1 FUNCTIONS/METHODS
+Given all the recent releases of C<*::Tiny> modules, I felt like I needed to
+get in on the action while there was still part of the namespace left.
 
-=begin :list
-
-* C<foo>
-
-=end :list
-
-...
+=for html <center><img src="https://github.com/karenetheridge/Tiny-Tiny/raw/images/tiny-tim_lg.jpg" alt="Tiny Tim" width="216" height="267"/> <img src="https://github.com/karenetheridge/Tiny-Tiny/raw/images/Elton_John_Tiny_Dancer.jpg" alt="Tiny Dancer" width="260" height="320"/></center>
 
 =head1 SUPPORT
 
@@ -33,12 +28,18 @@ Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Di
 (or L<bug-Tiny-Tiny@rt.cpan.org|mailto:bug-Tiny-Tiny@rt.cpan.org>).
 I am also usually active on irc, as 'ether' at C<irc.perl.org>.
 
-=head1 ACKNOWLEDGEMENTS
-
-...
-
 =head1 SEE ALSO
 
-...
+There are many excellent (and legitimate) *::Tiny modules out there,
+including:
+
+=begin :list
+
+* L<HTTP::Tiny>
+* L<Try::Tiny>
+* L<Role::Tiny>
+* L<Type::Tiny>
+
+=end :list
 
 =cut

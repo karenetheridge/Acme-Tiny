062a3d5ac87058200c55b4ebe604c2ecc299d7fa
Karen Etheridge <ether@cpan.org>
2013-04-21 13:58:47 -0700

    initial commit, via [Git::Init] in Dist-Zilla-PluginBundle-Author-ETHER 0.003, github profile.

---
+++ lib/Tiny/Tiny.pm
@@ -0,0 +1,44 @@
+use strict;
+use warnings;
+package Tiny::Tiny;
+# ABSTRACT: ...
+
+
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+...
+
+=head1 DESCRIPTION
+
+
+=head1 FUNCTIONS/METHODS
+
+=begin :list
+
+* C<foo>
+
+=end :list
+
+...
+
+=head1 SUPPORT
+
+Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Tiny-Tiny>
+(or L<bug-Tiny-Tiny@rt.cpan.org|mailto:bug-Tiny-Tiny@rt.cpan.org>).
+I am also usually active on irc, as 'ether' at C<irc.perl.org>.
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+...
+
+=cut
